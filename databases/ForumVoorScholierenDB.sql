0e7ee3352d1fffdb4c8614d70720131b59e5ccf0
RodinSmedes <152068@csgaugustinus.nl>
2023-04-01 14:41:52 +0000

    SQL

--- databases/ForumVoorScholierenDB.sql
+++ databases/ForumVoorScholierenDB.sql
@@ -6,7 +6,7 @@ CREATE TABLE `accounts` (
   `fldMail` varchar(50) NOT NULL,
   `fldWachtwoord` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 INSERT INTO `accounts` (`id`, `fldName`, `fldMail`, `fldWachtwoord`) VALUES (2,'Rodin','rodin@smedes.nl','Sterk');
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -15,6 +15,6 @@ CREATE TABLE `post` (
   `id` int NOT NULL AUTO_INCREMENT,
   `fldPost` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-INSERT INTO `post` (`id`, `fldPost`) VALUES (14,'Heeft iemand de antwoorden van Moderne Wiskunde A VWO 6 hoofdstuk 5?');
+INSERT INTO `post` (`id`, `fldPost`) VALUES (14,'Heeft iemand de antwoorden van Moderne Wiskunde A VWO 6 hoofdstuk 5?'),(20,'Wat is een conjuctuurschommeling?');
