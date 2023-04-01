9494c3e2be21a96ad07b0e45cec52e4d1bd400c5
RodinSmedes <152068@csgaugustinus.nl>
2023-04-01 21:29:46 +0000

    SQl2

--- databases/ForumVoorScholierenDB.sql
+++ databases/ForumVoorScholierenDB.sql
@@ -6,15 +6,16 @@ CREATE TABLE `accounts` (
   `fldMail` varchar(50) NOT NULL,
   `fldWachtwoord` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-INSERT INTO `accounts` (`id`, `fldName`, `fldMail`, `fldWachtwoord`) VALUES (2,'Rodin','rodin@smedes.nl','Sterk');
+INSERT INTO `accounts` (`id`, `fldName`, `fldMail`, `fldWachtwoord`) VALUES (2,'Rodin','rodin@smedes.nl','Sterk'),(54,'Dries','driemaalsmedes@gmail.com','Smint');
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `post` (
   `id` int NOT NULL AUTO_INCREMENT,
   `fldPost` text NOT NULL,
+  `fldName` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-INSERT INTO `post` (`id`, `fldPost`) VALUES (14,'Heeft iemand de antwoorden van Moderne Wiskunde A VWO 6 hoofdstuk 5?'),(20,'Wat is een conjuctuurschommeling?');
+INSERT INTO `post` (`id`, `fldPost`, `fldName`) VALUES (29,'Heeft iemand de antwoorden van Moderne Wiskunde A VWO 6 hoofdstuk 5?','\r\n            Rodin'),(30,'Wat is een conjuctuurschommeling?','\r\n            Dries');
